a55894bed85e5e6c9d7e2ad4532cb212481635cf
Rafael Corradini da Cunha <rafael.corradini@luizalabs.com>
2020-08-26 11:42:08 -0300

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name='flask-rest-crud',         
   packages=find_packages(),   
-  version='0.1.0',      
+  version='0.2.0',      
   license='MIT',        
   description='Generate full CRUD from mongoengine models',   
   author='Rafael Corradini da Cunha',                   
