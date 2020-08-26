95132ee5df2b269ff15202e3ff3f2da31f342f1d
Rafael Corradini da Cunha <rafael.corradini@luizalabs.com>
2020-08-26 10:23:43 -0300

    setup

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name='flask-rest-crud',         
   packages=find_packages(),   
-  version='0.0.1',      
+  version='0.1.0',      
   license='MIT',        
   description='Generate full CRUD from mongoengine models',   
   author='Rafael Corradini da Cunha',                   
   author_email='rafacunhadini@gmail.com',      
   url='https://github.com/rafaelcorradini/flask-rest-crud',   
-  keywords=['flask', 'mongoengine', 'crud', 'rest'],   
+  keywords=['flask', 'mongoengine', 'crud', 'rest'],
+  long_description=long_description,
+  long_description_content_type='text/markdown',  
   install_requires=[            
     'flask',
     'mongoengine'
